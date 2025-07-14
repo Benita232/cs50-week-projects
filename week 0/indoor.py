20d3d8e05842b49c105854ee4409828ce34351b1
Benita232 <benitannabuife1@gmail.com>
2025-07-14 20:53:33 +0200

    Done

--- week 0/indoor.py
+++ week 0/indoor.py
@@ -1,2 +1,5 @@
+''''
 text= input('what is your name: ') #converting user text to lowercase
 print(text.lower())
+'''
+print("hello"+"world")
--- week 0/playback.py
+++ week 0/playback.py
@@ -1,2 +1,9 @@
+
 user= input()
 print("...".join(user.split(" "))) #changing the spaces in between user output to ...
+"""
+user= input()
+user_modify= user.replace(" ", "...")
+
+print(user_modify)
+"""
